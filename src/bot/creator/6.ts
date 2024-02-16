668e60a062b3b679c87132054baca7362c3705e4
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-02-16 22:24:43 +0530

    Added template 6

---
+++ src/bot/creator/6.ts
@@ -0,0 +1,135 @@
+import { Steps } from "./steps";
+
+export const template6Steps: Steps = {
+  1: {
+    setting: "hero section's title",
+    image: "./step-images/6/1.png",
+    selector: "#hero-text",
+    inputType: "text",
+  },
+  2: {
+    setting: "hero section's description",
+    image: "./step-images/6/2.png",
+    selector: "#hero-description",
+    inputType: "text",
+  },
+  3: {
+    setting: "hero section's button text",
+    image: "./step-images/6/3.png",
+    selector: "#hero-btn",
+    inputType: "text",
+  },
+  4: {
+    setting: "hero section's button link",
+    image: "./step-images/6/3.png",
+    selector: "#hero-btn",
+    inputType: "link",
+  },
+  5: {
+    setting: "website's logo",
+    image: "./step-images/6/4.png",
+    selector: "#logo",
+    inputType: "image",
+  },
+  6: {
+    setting: "second section's title",
+    image: "./step-images/6/5.png",
+    selector: "#second-title",
+    inputType: "text",
+  },
+  7: {
+    setting: "first feature's image",
+    image: "./step-images/6/6.png",
+    selector: "#feature1-img",
+    inputType: "image",
+  },
+  8: {
+    setting: "first feature's title",
+    image: "./step-images/6/7.png",
+    selector: "#feature1-title",
+    inputType: "text",
+  },
+  9: {
+    setting: "first feature's description",
+    image: "./step-images/6/8.png",
+    selector: "#feature1-description",
+    inputType: "text",
+  },
+  10: {
+    setting: "second feature's image",
+    image: "./step-images/6/9.png",
+    selector: "#feature2-img",
+    inputType: "image",
+  },
+  11: {
+    setting: "second feature's title",
+    image: "./step-images/6/10.png",
+    selector: "#feature2-title",
+    inputType: "text",
+  },
+  12: {
+    setting: "second feature's description",
+    image: "./step-images/6/11.png",
+    selector: "#feature2-description",
+    inputType: "text",
+  },
+  13: {
+    setting: "third feature's image",
+    image: "./step-images/6/12.png",
+    selector: "#feature3-img",
+    inputType: "image",
+  },
+  14: {
+    setting: "third feature's title",
+    image: "./step-images/6/13.png",
+    selector: "#feature3-title",
+    inputType: "text",
+  },
+  15: {
+    setting: "third feature's description",
+    image: "./step-images/6/14.png",
+    selector: "#feature3-description",
+    inputType: "text",
+  },
+  16: {
+    setting: "third section's title",
+    image: "./step-images/6/15.png",
+    selector: "#third-title",
+    inputType: "text",
+  },
+  17: {
+    setting: "third section's description",
+    image: "./step-images/6/16.png",
+    selector: "#third-description",
+    inputType: "text",
+  },
+  18: {
+    setting: "third section's button text",
+    image: "./step-images/6/17.png",
+    selector: "#third-btn",
+    inputType: "text",
+  },
+  19: {
+    setting: "third section's button link",
+    image: "./step-images/6/17.png",
+    selector: "#third-btn",
+    inputType: "link",
+  },
+  20: {
+    setting: "third section's small title",
+    image: "./step-images/6/18.png",
+    selector: "#third-small-title",
+    inputType: "text",
+  },
+  21: {
+    setting: "third section's image",
+    image: "./step-images/6/19.png",
+    selector: "#third-img",
+    inputType: "image",
+  },
+  22: {
+    setting: "website title",
+    selector: "title",
+    inputType: "text",
+  },
+};
--- src/bot/creator/steps.ts
+++ src/bot/creator/steps.ts
@@ -3,6 +3,7 @@ import { template2Steps } from "./2";
 import { template3Steps } from "./3";
 import { template4Steps } from "./4";
 import { template5Steps } from "./5";
+import { template6Steps } from "./6";
 
 export interface Step {
   setting: string;
@@ -24,4 +25,5 @@ export const templateSteps: TemplateSteps = {
   3: template3Steps,
   4: template4Steps,
   5: template5Steps,
+  6: template6Steps,
 };
