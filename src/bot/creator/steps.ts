c6d468ff7c836f95c90f64f76045cd734f00a5d5
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-02-16 20:06:19 +0530

    Template 4 added

--- src/bot/creator/steps.ts
+++ src/bot/creator/steps.ts
@@ -1,6 +1,7 @@
 import { template1Steps } from "./1";
 import { template2Steps } from "./2";
 import { template3Steps } from "./3";
+import { template4Steps } from "./4";
 
 export interface Step {
   setting: string;
@@ -20,4 +21,5 @@ export const templateSteps: TemplateSteps = {
   1: template1Steps,
   2: template2Steps,
   3: template3Steps,
+  4: template4Steps,
 };
