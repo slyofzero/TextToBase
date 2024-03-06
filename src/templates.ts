31e2fc68f78951ac9ccbfbf94c7c22ced1e1fa88
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-03-06 12:13:25 +0530

    New previews

--- src/templates.ts
+++ src/templates.ts
@@ -13,7 +13,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template1",
     category: "Business, Crypto",
     estimatedTime: "5 minutes",
-    preview: "https://website-ai-previews.vercel.app/1",
+    preview: "https://aurora-previews.vercel.app/1",
     details:
       "Perfect for tokens. Features: Dynamic visual design with contrasting shades. Intuitive navigation with a clear home button and menu. Central globe-themed graphic ideal for tech or global businesses. Adaptable 'Title' and 'Description' sections. Mobile-responsive, SEO-friendly, and optimized for fast loading.",
   },
@@ -22,7 +22,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template2",
     category: "Crypto",
     estimatedTime: "5 minutes",
-    preview: "https://website-ai-previews.vercel.app/2",
+    preview: "https://aurora-previews.vercel.app/2",
     details:
       "Modern aesthetics with a deep blue palette. Fluid abstract shapes convey a sense of movement and innovation. Central content placement with clearly defined 'Title' and 'Description' sections. Two distinct call-to-action buttons for varied user engagement. Minimalistic footer with essential social icons. Sleek and contemporary design ideal for tech companies or creative agencies.",
   },
@@ -31,7 +31,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template3",
     category: "Crypto, Business",
     estimatedTime: "5 minutes",
-    preview: "https://website-ai-previews.vercel.app/3",
+    preview: "https://aurora-previews.vercel.app/3",
     details:
       "Sophisticated dark-themed landing template tailored for startups. Emphasizes a responsive design ensuring optimal viewing across all devices. Bold header with compelling call-to-actions, seamlessly leading to vivid geometric graphics. Features modular sections showcasing benefits, punctuated by crisp, blue-toned visuals. Footer culminates with a decisive nudge for viewer engagement. An epitome of elegance and functionality, ideal for tech startups and innovative businesses.",
   },
@@ -40,7 +40,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template4",
     category: "Software, Crypto, Business",
     estimatedTime: "5 Minutes",
-    preview: "https://website-ai-previews.vercel.app/4",
+    preview: "https://aurora-previews.vercel.app/4",
     details:
       "A serene and contemporary website layout set against a calming blue backdrop. Top features the recognizable logo and essential navigation links. Central focus is drawn to the intricately designed digital globe emblem, illustrating interconnectedness and tech-forward appeal. The page offers minimalist headers and sections to insert relevant descriptions and call-to-actions, complemented by intuitive icons. The simplicity of this design underscores its versatility and user-friendly nature, making it a prime choice for tech businesses and modern digital platforms.",
   },
@@ -49,7 +49,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template5",
     category: "Coming Soon",
     estimatedTime: "3 Minutes",
-    preview: "https://website-ai-previews.vercel.app/5",
+    preview: "https://aurora-previews.vercel.app/5",
     details:
       "A serene and contemporary website layout set against a calming blue backdrop. Top features the recognizable logo and essential navigation links. Central focus is drawn to the intricately designed digital globe emblem, illustrating interconnectedness and tech-forward appeal. The page offers minimalist headers and sections to insert relevant descriptions and call-to-actions, complemented by intuitive icons. The simplicity of this design underscores its versatility and user-friendly nature, making it a prime choice for tech businesses and modern digital platforms.",
   },
@@ -58,7 +58,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template6",
     category: "Crypto",
     estimatedTime: "3 Minutes",
-    preview: "https://website-ai-previews.vercel.app/6",
+    preview: "https://aurora-previews.vercel.app/6",
     details:
       "A sleek and modern website layout bathed in deep blue tones. The brand name prominently sits atop, signaling a cryptocurrency or financial tech theme. A dynamic wave pattern, transitioning from deep blue to a vibrant purple, flows across the page, symbolizing fluidity and digital transformation. The central headline, 'The Digital Asset for Payments,' accompanied by a succinct description, immediately informs visitors of the platform's core offering. The design manages to convey a sense of cutting-edge technology and trustworthiness, ideal for platforms operating within the digital finance domain.",
   },
@@ -67,7 +67,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template7",
     category: "Crypto",
     estimatedTime: "5 Minutes",
-    preview: "https://website-ai-previews.vercel.app/7",
+    preview: "https://aurora-previews.vercel.app/7",
     details:
       "A vibrant website layout bathed in rich shades of purple and deep blue, exuding a futuristic and tech-savvy ambiance. Dominating the scene are striking isometric illustrations, including a sleek laptop emanating golden cryptocurrency symbols, a modern smartphone, and distinct pedestals showcasing various digital coins. Flanking these illustrations are three minimalist icons representing technology, fairness, and global trust, each set against the deep backdrop. The overall design seamlessly blends a sense of innovation with the world of decentralized finance, making it highly appealing to tech enthusiasts and modern-day investors. The layout is clean, well-spaced, and gives a sense of cutting-edge sophistication.",
   },
@@ -76,7 +76,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template8",
     category: "Coming Soon",
     estimatedTime: "1 Minute",
-    preview: "https://website-ai-previews.vercel.app/8",
+    preview: "https://aurora-previews.vercel.app/8",
     details:
       "A visually captivating website design set against a cosmic backdrop. The top left features a complex, silver-white constellation of interconnected lines and dots, reminiscent of a starry network or digital constellation. The vast expanse of the background is a deep, star-speckled navy, gradually transitioning into waves of flowing, vibrant colors on the right. These colors include luminescent shades of pink, magenta, violet, and deep blue, evoking the visual splendor of celestial nebulas or the Northern Lights.",
   },
@@ -85,7 +85,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template9",
     category: "Crypto",
     estimatedTime: "8 Minutes",
-    preview: "https://website-ai-previews.vercel.app/9",
+    preview: "https://aurora-previews.vercel.app/9",
     details:
       "A clean and very elegant design for startups. Works on all devices.",
   },
@@ -94,7 +94,7 @@ export const templateDescriptions: TemplateDescription[] = [
     name: "template10",
     category: "Crypto",
     estimatedTime: "2 Minutes",
-    preview: "https://website-ai-previews.vercel.app/10",
+    preview: "https://aurora-previews.vercel.app/10",
     details: "A simple design with no need for icons.",
   },
 ];
